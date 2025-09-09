df59ddffc1bb548a4775fb58d72c9c7dad00128e
lisadelbes <lisadelbes@hotmail.fr>
2025-09-09 12:11:21 +0200

    création exo Vue et Nuxt

---
+++ projet-cours-vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+      <p> {{ toto }} </p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup lang="js">
+
+const toto = "Lisa DELBES";
+
+</script>
+
+<style scoped>
+</style>
