404c867fc0d46d8ff6f0cf329feab90f6ab4c011
lisadelbes <lisadelbes@hotmail.fr>
2025-09-10 16:25:01 +0200

    nouveaux fichiers

---
+++ projet-cours-vue/src/views/NotFoundView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="not-found">
+  <div class="error">404</div>
+  <br /><br />
+  <span class="info">File not found</span>
+  <img src="http://images2.layoutsparks.com/1/160030/too-much-tv-static.gif" class="static" />
+</div>
+</template>
